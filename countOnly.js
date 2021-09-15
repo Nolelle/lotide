fa261cd131eb6dcf8adbc3c77c67813dec547502
Edmund Yu <eyu1@ualberta.ca>
2021-09-15 22:34:32 +0000

    Implemented countOnly function.

---
+++ countOnly.js
@@ -0,0 +1,51 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function (allItems, itemsToCount) {
+  //allitems is an array, itemstocount is an obj.
+  const results = {};
+
+  for (const item of allItems) {
+    //console.log("Items: ", item);
+    console.log(results);
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  //Returns obj continaing counts of everything that the input object listed.
+  return results;
+};
+
+//Test Code
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
