c1a57d50c5dccc4b255bd73b69a29b3eef1b51bf
Edmund Yu <eyu1@ualberta.ca>
2021-09-16 00:35:10 +0000

    Implemented countLetters function.

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(` ✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  //input sentence is a string
+  const result = {};
+  for (let letter of sentence) {
+    //console.log(letter);
+    if (letter === " ") {
+      continue;
+    }
+    if (result[letter] === undefined) {
+      result[letter] = 1;
+    } else {
+      result[letter] += 1;
+    }
+  }
+  //return a count of each letters appreance in a sentence.
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
