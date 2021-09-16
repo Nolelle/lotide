1ec2e713b14c25c2af359a96342b2fd568a49752
Edmund Yu <eyu1@ualberta.ca>
2021-09-16 01:10:51 +0000

    added asserEqual test cases.

--- countLetters.js
+++ countLetters.js
@@ -25,3 +25,5 @@ const countLetters = function (sentence) {
 };
 
 console.log(countLetters("lighthouse in the house"));
+assertEqual(countLetters("lighthouse in the house").h, 4);
+assertEqual(countLetters("lighthouse in the house").e, 3);
