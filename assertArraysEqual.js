0e1b4baa9bf536cbe41af33825659437bc6e0a60
Edmund Yu <eyu1@ualberta.ca>
2021-09-20 16:09:56 +0000

    Refactored assertArraysEqual to move require eqArrays and export the assertArraysEqual function.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("../eqArrays");
 
 const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -14,3 +7,5 @@ const assertArraysEqual = function (actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+module.exports = assertArraysEqual;
